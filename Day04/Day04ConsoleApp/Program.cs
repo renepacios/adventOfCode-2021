b658734dfff8265dee8e6433c2ff4bfe3d478b0b
René Pacios <3912951+renepacios@users.noreply.github.com>
2021-12-04 12:54:58 +0100

    day 04 started

---
+++ Day04/Day04ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using AdventUtils;
+using System;
+
+namespace Day04ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+            PartTwo();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        private static void PartTwo()
+        {
+            var fileData = DataReader.Read("data.txt", s => (s));
+
+            string oxygen = string.Empty,
+                co2 = string.Empty;
+
+            Console.WriteLine($"There are oxygen {oxygen } binary value co2 {co2} binary value . The decimal is {Convert.ToInt32(oxygen, 2)} oxygen and {Convert.ToInt32(co2, 2)}  co2. The Product is  {Convert.ToInt32(oxygen, 2) * Convert.ToInt32(co2, 2)}");
+        }
+
+        private static void PartOne()
+        {
+            var fileData = DataReader.Read("data.txt", s => (s));
+
+            string oxygen = string.Empty,
+                co2 = string.Empty;
+
+            Console.WriteLine($"There are oxygen {oxygen } binary value co2 {co2} binary value . The decimal is {Convert.ToInt32(oxygen, 2)} oxygen and {Convert.ToInt32(co2, 2)}  co2. The Product is  {Convert.ToInt32(oxygen, 2) * Convert.ToInt32(co2, 2)}");
+        }
+    }
+}
